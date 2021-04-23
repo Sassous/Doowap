ad3abf3ad4a7f6841c23b01fe255421b489c6bdf
Sassous <sarah.mirouze@gmail.com>
2021-04-23 10:44:37 +0200

    doowap 78

--- js/scriptAstro.js
+++ js/scriptAstro.js
@@ -86,3 +86,107 @@ function setGenre() {
         tagsEl.append(t);
     })
 }
+function highlightSelection() {
+    const tags = document.querySelectorAll('.tag');
+    tags.forEach(tag => {
+        tag.classList.remove('highlight')
+    })
+    clearBtn()
+    if(selectedGenre.length !=0){   
+        selectedGenre.forEach(id => {
+            const hightlightedTag = document.getElementById(id);
+            hightlightedTag.classList.add('highlight');
+        })
+    }
+
+}
+
+function clearBtn(){
+    let clearBtn = document.getElementById('clear');
+    if(clearBtn){
+        clearBtn.classList.add('highlight')
+    }else{
+            
+        let clear = document.createElement('div');
+        clear.classList.add('tag','highlight');
+        clear.id = 'clear';
+        clear.innerText = 'Clear x';
+        clear.addEventListener('click', () => {
+            selectedGenre = [];
+            setGenre();            
+            getMovies(API_URL);
+        })
+        tagsEl.append(clear);
+    }
+    
+}
+
+getMovies(API_URL);
+
+function getMovies(url) {
+
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        if(data.results.length !== 0){
+            showMovies(data.results);
+        }else{
+            main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
+        }
+         
+       
+    })
+
+}
+
+randomJoke(data.results);
+    var joke = data.results[Math.floor(Math.random()*data.results.length)];
+    this.setState({jokes: [joke] });
+
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+             <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        
+        `
+
+        main.appendChild(movieEl);
+    })
+}
+
+
+function getColor(vote) {
+    if(vote>= 8){
+        return 'green'
+    }else if(vote >= 5){
+        return "orange"
+    }else{
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    selectedGenre=[];
+    setGenre();
+    if(searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(API_URL);
+    }
+
+})
--- js/scriptPop.js
+++ js/scriptPop.js
@@ -1,46 +1,34 @@
-
-const API_KEY = 'api_key=cc8a996114ccd712c62e4ea4975135a9';
+const API_KEY = 'cc8a996114ccd712c62e4ea4975135a9';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/movie/popular?api_key=${' + API_KEY + '}&language=en-US';
+const API_URL = BASE_URL + '/movie/popular?api_key=' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?'+API_KEY;
-
 const main = document.getElementById('main');
+/*getPopular();
+function getPopular() {
+  fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US`
+  )
+    .then((result) => result.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch(function (err) {
+      alert(err);
+    });
+  }*/
 const form =  document.getElementById('form');
-const search = document.getElementById('search');
-const tagsEl = document.getElementById('tags');
 
-function clearBtn(){
-    let clearBtn = document.getElementById('clear');
-    if(clearBtn){
-        clearBtn.classList.add('highlight')
-    }else{
-            
-        let clear = document.createElement('div');
-        clear.classList.add('tag','highlight');
-        clear.id = 'clear';
-        clear.innerText = 'Clear x';
-        clear.addEventListener('click', () => {
-            selectedGenre = [];
-            setGenre();            
-            getMovies(API_URL);
-        })
-        tagsEl.append(clear);
-    }
-    
-}
 
-getMovies(API_URL);
+getPopular(API_URL);
 
-function getMovies(url) {
+function getPopular(API_URL) {
 
-    fetch(url).then(res => res.json()).then(data => {
+    fetch(API_URL)
+    .then((result) => result.json())
+    .then((data) => {
         console.log(data.results)
-        if(data.results.length !== 0){
             showMovies(data.results);
-        }else{
-            main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
-        }
+        
        
     })
 
@@ -49,7 +37,7 @@ function getMovies(url) {
 
 function showMovies(data) {
     main.innerHTML = '';
-
+ console.log(data);
     data.forEach(movie => {
         const {title, poster_path, vote_average, overview} = movie;
         const movieEl = document.createElement('div');
@@ -81,17 +69,3 @@ function getColor(vote) {
         return 'red'
     }
 }
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const searchTerm = search.value;
-    selectedGenre=[];
-    setGenre();
-    if(searchTerm) {
-        getMovies(searchURL+'&query='+searchTerm)
-    }else{
-        getMovies(API_URL);
-    }
-
-})
